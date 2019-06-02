2b8cd8536e4dd196dd1f09f58d812066768c9108
moniet <moniet.sawhney@gmail.com>
2019-06-02 10:10:28 +0100

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_term = req.path.split("/items/").last
+      item = @@items.find {|item| item.name == search_term}
+
+      if item
+        resp.write "#{item.price}\n"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
